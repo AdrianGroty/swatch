f737d29538171722dd2684b1d4382d2a38fed298
AdrianGroty <40133867+AdrianGroty@users.noreply.github.com>
2025-01-04 09:15:27 -0600

    Update color.sh
    Reset color at end of each line.

--- color.sh
+++ color.sh
@@ -8,16 +8,19 @@ for C in {10..15}; do
     tput setab $C
     echo -n "$C  "
 done
+tput sgr0
 echo
 for C in {16..51}; do
     tput setab $C
     echo -n "$C  "
 done
+tput sgr0
 echo
 for C in {52..87}; do
     tput setab $C
     echo -n "$C  "
 done
+tput sgr0
 echo
 for C in {88..99}; do
     tput setab $C
@@ -27,21 +30,25 @@ for C in {100..123}; do
     tput setab $C
     echo -n "$C "
 done
+tput sgr0
 echo
 for C in {124..159}; do
     tput setab $C
     echo -n "$C "
 done
+tput sgr0
 echo
 for C in {160..195}; do
     tput setab $C
     echo -n "$C "
 done
+tput sgr0
 echo
 for C in {196..231}; do
     tput setab $C
     echo -n "$C "
 done
+tput sgr0
 echo
 for C in {232..255}; do
     tput setab $C
